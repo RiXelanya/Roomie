ae72b131d5aed9d1a34f1a18a637dd044f32e91a
RXRD <lilixinxin@protonmail.com>
2023-02-15 16:49:20 +0700

    new document

--- routes/record.js
+++ routes/record.js
@@ -11,8 +11,8 @@ Routes.route("/tenants").get(async function (req, res) {
 
   dbConnect
     .collection("room")
-    .find({availability: false}).sort({room_id: 1}) // find all occupied room room and sort by room id
-    .project({_id : 0 , room_id: 1, name: 1}) // then only show room id and tenant name
+    .find({availability: false}).sort({_id: 1}) // find all occupied room room and sort by room id
+    .project({_id: 1, name: 1}) // then only show room id and tenant name
     .toArray(function (err, result) {
       if (err) {
         res.status(400).send("Error fetching Tenant List");
@@ -26,6 +26,34 @@ Routes.route("/tenants").get(async function (req, res) {
   }
 });
 
+Routes.route("/update/add").get(function(req, res){
+  res.sendFile('add.html'); // checkin webpage
+});
+
+Routes.route("/update/add").post(function (req, res) {
+  // add new room
+  if(req.session.user) { // make sure admin has logged in
+  const dbConnect = dbo.getDb(); // connect to database
+  const matchDocument = {
+    _id: req.body.id,
+    availability: true
+  }; // json info of the document to be modified
+  dbConnect
+    .collection("room")
+    .insertOne(matchDocument, function (err, result) { // replace the document
+      if (err) {
+        res.status(400).send("Error Adding room!");
+      } else {
+        console.log(`Room ${req.body.id} has been added`);
+        res.status(204).send();
+      }
+    });
+  }
+  else {
+    res.redirect("/login"); // redirected to login if has not logged in yet
+  }
+});
+
 Routes.route("/update/checkin").get(function(req, res){
   res.sendFile('checkin.html'); // checkin webpage
 });
@@ -35,13 +63,13 @@ Routes.route("/update/checkin").post(function (req, res) {
   if(req.session.user) { // make sure admin has logged in
   const dbConnect = dbo.getDb(); // connect to database
   const matchDocument = {
-    room_id: req.body.id,
+    _id: req.body.id,
     checkin_date: new Date(),
     name: req.body.name,
     email: req.body.email,
     availability: false
   }; // json info of the document to be modified
-  const filter = {room_id: req.body.id} ; // id is used to search for document to be replaced
+  const filter = {_id: req.body.id} ; // id is used to search for document to be replaced
   dbConnect
     .collection("room")
     .replaceOne(filter , matchDocument, function (err, result) { // replace the document
@@ -66,10 +94,10 @@ Routes.route("/update/checkout").post(function (req, res) {
   if(req.session.user) { // verify that admin has logged in
   const dbConnect = dbo.getDb(); // making connection to database
   const matchDocument = {
-    room_id: req.body.id,
+    _id: req.body.id,
     availability: true
   }; // the document info after checkout
-  const filter = {room_id: req.body.id} ; // search by room number 
+  const filter = {_id: req.body.id} ; // search by room number 
   dbConnect
     .collection("room")
     .replaceOne(filter , matchDocument, function (err, result) { // replace
